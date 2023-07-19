4913ae710967a616fb7634e2e58dfa1867062d00
HLOEY <default_crt@yahoo.com>
2023-07-19 14:24:46 +0700

    add screens

---
+++ screens/reviewDetail.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export default class reviewDetail extends Component {
+  render() {
+    return (
+      <View>
+        <Text>reviewDetail</Text>
+      </View>
+    );
+  }
+}
