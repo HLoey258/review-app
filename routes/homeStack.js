3a65223a7ba3d973b697317dd25d46b52f69fcad
HLOEY <default_crt@yahoo.com>
2023-07-21 19:04:00 +0700

    testing navigator

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,2 +1,17 @@
 // in this file i config my stack navigator
-import { createSwitchNavigator } from "react-navigation";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "react-navigation";
+import { View, Text } from "react-native";
+import React from "react";
+import Home from "../screens/home";
+const Stack = createNativeStackNavigator();
+
+export default function homeStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={Home}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
